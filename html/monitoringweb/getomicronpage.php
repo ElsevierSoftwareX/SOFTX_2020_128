ab0a453e4845798ce075481986ece729c81efe3e
Florent Robinet <robinet@lal.in2p3.fr>
2013-01-21 09:08:08 +0000

    omicron monitoring
    git-svn-id: https://svn.ego-gw.it/svn/advsw/Omicron/trunk@34633 47e613b6-93f3-4244-b775-e06b1756610f

---
+++ html/monitoringweb/getomicronpage.php
@@ -0,0 +1,128 @@
+<html>
+<head>
+<title>Omicron: your request</title>
+<link rel="stylesheet" href="style.css" type="text/css">
+<link rel="icon" type="image/x-icon" href="icon.gif" />
+<link rel="shortcut icon" type="image/x-icon" href="icon.gif" />
+</head>
+
+<body>
+    
+   <!-- check arguments from file -->
+    
+   <?php
+   $badNumber = "/([^0-9.])/";
+   include "myenv.php";
+
+   // get values
+   $timemode = $_REQUEST['timemode'];
+   $gpsvalue = trim($_REQUEST['gpsvalue']);
+   $year = $_REQUEST['year'];
+   $month = $_REQUEST['month'];
+   $day = $_REQUEST['day'];
+   $hour = $_REQUEST['hour'];
+   $predefvalue = $_REQUEST['predefvalue'];
+   $channel = trim($_REQUEST['channel']);
+   $fullday = $_REQUEST['fullday'];
+   
+   // Only list available channels
+   if($_REQUEST['getchannel'] == 'Get Available Channels'){
+     $chandirs = array();
+     
+     // loop over channel directories
+     if ($handle = opendir("{$OMICRONTRIGGERS}")) {
+       while (false !== ($chandir = readdir($handle))) {
+	 if(!strpos($chandir,'_')) continue;
+	 $chandirs[] = $chandir;
+       }
+       closedir($handle);
+     }
+     sort($chandirs);
+     echo "<pre>\n";
+     print_r($chandirs);
+     //foreach($chandir in $chandirs) echo "$chandir\n";
+     echo "</pre>\n";
+   }
+   else{
+     if($timemode=="date"){// ----------- Date input
+       if($hour=="all") $fullday="yes";
+       else $fullday="no";
+     }
+     else{
+       if($timemode=="gps"){// ----------- GPS time input
+	 if(empty($gpsvalue)) die("Please specify a GPS value");
+	 if(preg_match($badNumber, $gpsvalue)) die("Your GPS value does not make any sense");
+	 if($gpsvalue<700000000) die("Your GPS value does not make any sense");
+	 $tstamp=exec("source ./environment.sh; tconvert -f \"%Y %m %d %H\" {$gpsvalue}");
+       }
+       else{// ----------- Predefined time input
+	 if($predefvalue=="latestday"){// latest day
+	   if(file_exists("./latestday/{$channel}.html")){
+	     header("Location: ./latestday/{$channel}.html");
+	     exit;
+	   }
+	   else die("Channel {$channel} is not available for the last day");
+	 }
+	 if($predefvalue=="latesthour"){// latest hour
+	   if(file_exists("./latesthour/{$channel}.html")){
+	     header("Location: ./latesthour/{$channel}.html");
+	     exit;
+	   }
+	   else die("Channel {$channel} is not available for the last hour");
+	 }
+	 elseif($predefvalue=="thishour"){// this hour
+	   $gps_now=exec("source ./environment.sh; tconvert now");
+	   $tstamp=exec("source ./environment.sh; tconvert -f \"%Y %m %d %H\" {$gps_now}");
+	   $fullday="no";	 
+	 }
+	 else{// today
+	   $gps_now=exec("source ./environment.sh; tconvert now");
+	   $tstamp=exec("source ./environment.sh; tconvert -f \"%Y %m %d %H\" {$gps_now}");
+	   $fullday="yes";
+	 }
+       }
+       // convert gps into date elements
+       $date_elmt = explode(" ", $tstamp);
+       $year=$date_elmt[0];
+       $month=$date_elmt[1];
+       $day=$date_elmt[2];
+       $hour=$date_elmt[3];
+     }
+     
+     // redirect to existing page if it exists
+     if($fullday=="yes"){
+       if(file_exists("./{$year}/{$month}/{$day}/{$channel}.html")){
+	 header("Location: ./{$year}/{$month}/{$day}/{$channel}.html");
+	 exit;
+       }
+       else die("Your request ({$channel}: {$year}-{$month}-{$day}) does not lead to an existing web page.<br>Do you want to process your request anyway and create a web report on demand?<br>Well, be patient; this feature will be available later...");
+     }
+     else{
+       if(file_exists("./{$year}/{$month}/{$day}/{$hour}/{$channel}.html")){
+	 header("Location: ./{$year}/{$month}/{$day}/{$hour}/{$channel}.html");
+	 exit;
+       }
+       else die("Your request ({$channel}: {$year}-{$month}-{$day} {$hour}h) does not lead to an existing web page.<br>Do you want to process your request anyway and create a web report on demand?<br>Well, be patient; this feature will be available later...");
+     }
+
+   }
+   ?>  
+    
+
+  <!-- foot page -->
+  <table valign="bottom" align="center">
+    <tr>
+      <td valign="bottom" align="right">
+	<img src="./omicronlogo_s.gif" width="42">
+      </td>
+      <td valign="middle" align="left">
+	<p>
+	  Florent Robinet<br>
+	  <a HREF="mailto:robinet@lal.in2p3.fr">Contact: robinet@lal.in2p3.fr</a>
+	</p>
+      </td>
+    </tr>
+  </table>
+  
+</body>
+</html>
---
+++ html/monitoringweb/getomiscan.php
@@ -0,0 +1,86 @@
+<html>
+<head>
+<title>OmiScan: your scan</title>
+<link rel="stylesheet" href="style.css" type="text/css">
+<link rel="icon" type="image/x-icon" href="icon.gif" />
+<link rel="shortcut icon" type="image/x-icon" href="icon.gif" />
+</head>
+
+<body>
+    
+   <!-- check arguments -->
+    
+   <?php
+   include "myenv.php";
+   $badNumber = "/([^0-9.])/";
+
+   // get values
+   $gpscenter = $_REQUEST['gpscenter'];
+   $mainchannel = $_REQUEST['mainchannel'];
+   $thr = $_REQUEST['thr'];
+
+   // check gps value
+   if(empty($gpscenter)) die("Please specify a GPS value");
+   if(preg_match($badNumber, $gpscenter)) die("Your GPS value does not make any sense");
+   if($gpscenter<700000000) die("Your GPS value does not make any sense");
+
+   // check main channel
+   if(empty($mainchannel)) die("Please specify a main channel (h_4096Hz maybe?)");
+
+
+   die("Sorry, OmiScan can not be started from the web (yet!). Come back later");
+
+   // FIXME: I can't write scripts in any other directory
+   // output directory in /tmp
+   $outdir=$OMICRON."/scan";
+   $scriptdir="/opt/w3/DataAnalysis/VOD/output/script";
+
+   // command
+   $command=$GWOLLUM."/local/scripts/GetOmicronScan.sh -m ".$mainchannel." -d ".$outdir." -s ".$thr." ".$gpscenter." >> /dev/null";
+   $cleaningcommand1="find {$outdir}/ -maxdepth 1 -type d -mtime +3 -exec rm -r {} \;";
+   
+   // random key for unicity
+   $randomkey=rand();
+    
+   // command file	
+   $commandfile=$scriptdir."/omiscan.".$randomkey.".sh";
+     
+//echo exec("ls -l /data/procdata/bufferv16/Omicron/Omicron | grep scan");
+//echo exec("pwd");
+//echo exec("whoami");
+
+   // open command file and fill it
+   $fp=fopen($commandfile,'w') or die("can't open file {$commandfile}");
+   fwrite($fp,"#!/bin/bash\n");
+   fwrite($fp,$cleaningcommand1."\n");
+   fwrite($fp,$command."\n");
+   fwrite($fp,"exit 0\n");
+   fclose($fp);
+   chmod($commandfile,0777);
+
+   // redirect to existing page if it exists
+   if(file_exists("{$outdir}/{$gpscenter}/index.html")){
+       header("Location: {$outdir}/{$gpscenter}/index.html");
+       exit;
+   }
+   else die("Your Omiscan failed.");
+   ?>  
+    
+
+  <!-- foot page -->
+  <table valign="bottom" align="center">
+    <tr>
+      <td valign="bottom" align="right">
+	<img src="./omicronlogo_s.gif" width="42">
+      </td>
+      <td valign="middle" align="left">
+	<p>
+	  Florent Robinet<br>
+	  <a HREF="mailto:robinet@lal.in2p3.fr">Contact: robinet@lal.in2p3.fr</a>
+	</p>
+      </td>
+    </tr>
+  </table>
+  
+</body>
+</html>
