d2ec9e528d7cdab01a2fa846a99f68985a7280cb
Florent Robinet <robinet@lal.in2p3.fr>
2013-06-14 10:58:18 +0000

    add
    git-svn-id: https://svn.ego-gw.it/svn/advsw/Omicron/trunk@34721 47e613b6-93f3-4244-b775-e06b1756610f

---
+++ html/monitoringweb/getomicronchannels.php
@@ -0,0 +1,120 @@
+<html>
+<head>
+<title>Omicron channels</title>
+<link rel="stylesheet" href="style.css" type="text/css">
+<link rel="icon" type="image/x-icon" href="icon.gif" />
+<link rel="shortcut icon" type="image/x-icon" href="icon.gif" />
+</head>
+
+<body>
+
+<!-- check arguments from file -->
+
+<?php
+include "tconvert.php";
+$badNumber = "/([^0-9.])/";
+
+// set the default timezone to use: UTC
+date_default_timezone_set('UTC');
+
+// get values
+$timemode = $_REQUEST['timemode'];
+$gpsvalue = trim($_REQUEST['gpsvalue']);
+$year = $_REQUEST['year'];
+$month = $_REQUEST['month'];
+$day = $_REQUEST['day'];
+$hour = $_REQUEST['hour'];
+$predefvalue = $_REQUEST['predefvalue'];
+$fullday = $_REQUEST['fullday'];
+   
+// Only list available channels
+if($_REQUEST['getchannel'] == 'Get Available Channels'){
+  $chandirs = array();
+  
+  // loop over channel directories
+  if ($handle = opendir("{$OMICRONTRIGGERS}")) {
+    while (false !== ($chandir = readdir($handle))) {
+      if(!strpos($chandir,'_')) continue;
+      $chandirs[] = $chandir;
+    }
+    closedir($handle);
+  }
+  sort($chandirs);
+  echo "<pre>\n";
+  print_r($chandirs);
+  //foreach($chandir in $chandirs) echo "$chandir\n";
+  echo "</pre>\n";
+}
+
+if($timemode=="date"){// ----------- Date input
+  if($hour=="all") $fullday="yes";
+  else $fullday="no";
+}
+else{
+  if($timemode=="gps"){// ----------- GPS time input
+    if(empty($gpsvalue)) die("Please specify a GPS value");
+    if(preg_match($badNumber, $gpsvalue)) die("Your GPS value does not make any sense");
+    if($gpsvalue<700000000) die("Your GPS value does not make any sense");
+    $unixtime=gps2unix($gpsvalue);
+    $date_elmt = explode(" ",date("Y m d H",$unixtime));
+  }
+  else{// ----------- Predefined time input
+    if($predefvalue=="latestday"){// latest day
+      if(file_exists("./latestday/{$channel}.html")){
+	header("Location: ./latestday/{$channel}.html");
+	exit;
+      }
+      else die("Channel {$channel} is not available for the last day");
+    }
+    if($predefvalue=="latesthour"){// latest hour
+      if(file_exists("./latesthour/{$channel}.html")){
+	header("Location: ./latesthour/{$channel}.html");
+	exit;
+      }
+      else die("Channel {$channel} is not available for the last hour");
+    }
+    elseif($predefvalue=="thishour"){// this hour
+      $date_elmt = explode(" ",date("Y m d H"));
+      $fullday="no";
+    }
+    else{// today
+      $date_elmt = explode(" ",date("Y m d H"));
+      $fullday="yes";
+    }
+  }
+  
+  // get date elements
+  $year=$date_elmt[0];
+  $month=$date_elmt[1];
+  $day=$date_elmt[2];
+  $hour=$date_elmt[3];
+}
+  
+// list channels for this day
+if($fullday=="yes"){
+
+  foreach (glob("./{$year}/{$month}/{$day}/*.html") as $filename) {
+    echo "$filename size " . filesize($filename) . "<br>\n";
+  }
+  
+}
+?>  
+
+
+  <!-- foot page -->
+  <table valign="bottom" align="center">
+    <tr>
+      <td valign="bottom" align="right">
+	<img src="./omicronlogo_s.gif" width="42">
+      </td>
+      <td valign="middle" align="left">
+	<p>
+	  Florent Robinet<br>
+	  <a HREF="mailto:robinet@lal.in2p3.fr">Contact: robinet@lal.in2p3.fr</a>
+	</p>
+      </td>
+    </tr>
+  </table>
+  
+</body>
+</html>
