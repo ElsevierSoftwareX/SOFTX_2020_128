e81724457cde6f2b5da7f680fb8d7318466976eb
Florent Robinet <robinet@lal.in2p3.fr>
2014-09-10 12:51:36 +0000

    remove template and add online option
    git-svn-id: https://svn.ego-gw.it/svn/advsw/Omicron/trunk@56348 47e613b6-93f3-4244-b775-e06b1756610f

--- scripts/GetOmicronOptions.sh
+++ scripts/GetOmicronOptions.sh
@@ -12,8 +12,6 @@ printhelp(){
     echo ""
     echo "This script produces Omicron parameter files."
     echo ""
-    echo "  -o  [OPTION FILE]   path to an Omicron parameter file used as a template"
-    echo "                      Default: use a standardized parameter file"
     echo "  -c  [CHANNEL_FILE]  path to a text file listing all the channels to process"
     echo "                      Different settings are available and are specified by"    
     echo "                      a keyword given after the channel name:"
@@ -24,10 +22,13 @@ printhelp(){
     echo "                         FINE: 8    to 4096Hz, SNR>6, fine tiling"
     echo "                         GW:   32   to 4096Hz, SNR>5, fine tiling"
     echo ""
+    echo "  -o                     flag for online analyses"
+    echo "                         chunks are forced ro 16s and overlaps to 2s"
+    echo "                         (except for LOW)"
     echo ""
 
-    echo "  -d  [TRIG_OUTDIR]   trigger output directory (inactive if option file given)"
-    echo "  -f  [FFL_FILE]      path to a FFL/LCF file to consider (inactive if option file given)"
+    echo "  -d  [TRIG_OUTDIR]   trigger output directory"
+    echo "  -f  [FFL_FILE]      path to a FFL/LCF file to consider"
     echo ""
     echo "  -X                  add XML output format with time clustering"
     echo ""
@@ -37,9 +38,13 @@ printhelp(){
 
 printoption(){
     if [ $1 = "LOW" ]; then
+	# online and offline configs are the same
 	sampling=128
 	freqmin=0.1
 	freqmax=64
+	chunk=2048
+	block=2048
+	overlap=48
 	chunk=2048
 	block=2048
 	overlap=48
@@ -49,76 +54,110 @@ printoption(){
 	sampling=16384
 	freqmin=1024
 	freqmax=8192
-	chunk=506
-	block=256
-	overlap=6
+	if [ $4 -eq 0 ]; then # offline
+	    chunk=506
+	    block=256
+	    overlap=6
+	else                  # online
+	    chunk=16
+	    block=16
+	    overlap=2
+	fi
 	mmmax=0.35
 	snrthr=8	
     elif [ $1 = "GW" ]; then # this was optimized for ligo scripts
 	sampling=8192
 	freqmin=32
 	freqmax=4096
-	chunk=314
-	block=64
-	overlap=14
+	if [ $4 -eq 0 ]; then # offline
+	    chunk=314
+	    block=64
+	    overlap=14
+	else                  # online
+	    chunk=16
+	    block=16
+	    overlap=2
+	fi
 	mmmax=0.2
 	snrthr=5	
     elif [ $1 = "FINE" ]; then # this was optimized for ligo scripts
 	sampling=8192
 	freqmin=8
 	freqmax=4096
-	chunk=314
-	block=64
-	overlap=14
+	if [ $4 -eq 0 ]; then # offline
+	    chunk=314
+	    block=64
+	    overlap=14
+	else                  # online
+	    chunk=16
+	    block=16
+	    overlap=2
+	fi
 	mmmax=0.2
 	snrthr=6	
     elif [ $1 = "STD2" ]; then
 	sampling=2048
 	freqmin=8
 	freqmax=1024
-	chunk=506
-	block=256
-	overlap=6
+	if [ $4 -eq 0 ]; then # offline
+	    chunk=506
+	    block=256
+	    overlap=6
+	else                  # online
+	    chunk=16
+	    block=16
+	    overlap=2
+	fi
 	mmmax=0.35
 	snrthr=8
     else
 	sampling=4096
 	freqmin=8
 	freqmax=2048
-	chunk=506
-	block=256
-	overlap=6
+	if [ $4 -eq 0 ]; then # offline
+	    chunk=506
+	    block=256
+	    overlap=6
+	else                  # online
+	    chunk=16
+	    block=16
+	    overlap=2
+	fi
 	mmmax=0.35
 	snrthr=8
     fi
     	
-    sed -e '/DATA[ \t]*CHANNELS/d' \
-	-e '/DATA[ \t]*SAMPLEFREQUENCY/d' \
-	-e '/PARAMETER[ \t]*CHUNKDURATION/d' \
-	-e '/PARAMETER[ \t]*BLOCKDURATION/d' \
-	-e '/PARAMETER[ \t]*OVERLAPDURATION/d' \
-	-e '/PARAMETER[ \t]*FREQUENCYRANGE/d' \
-	-e '/PARAMETER[ \t]*MISMATCHMAX/d' \
-	-e '/TRIGGER[ \t]*SNRTHRESHOLD/d' \
-	-e '/OUTPUT[ \t]*FORMAT/d' \
-	-e '/TRIGGER[ \t]*CLUSTERING/d' \
-	$2 > ./parameters_${1}_${3}.txt
+    # Static parameters
+    echo "// ------------------------------------------------------------------"  > ./parameters_${1}_${2}.txt
+    echo "// Omicron option file generated on `date`"                            >> ./parameters_${1}_${2}.txt
+    echo "// Configuration type = $1"                                            >> ./parameters_${1}_${2}.txt
+    echo "// ------------------------------------------------------------------" >> ./parameters_${1}_${2}.txt
+    echo ""                                                                      >> ./parameters_${1}_${2}.txt
+    echo "PARAMETER  QRANGE           3.3166  100.0"                             >> ./parameters_${1}_${2}.txt
+    echo "PARAMETER  RATEMAX          100"                                       >> ./parameters_${1}_${2}.txt
+    echo "OUTPUT     FORMAT           root"                                      >> ./parameters_${1}_${2}.txt
+    echo "OUTPUT     VERBOSITY        0"                                         >> ./parameters_${1}_${2}.txt
+    echo "OUTPUT     WRITEPSD         0"                                         >> ./parameters_${1}_${2}.txt
+    echo "OUTPUT     WRITETIMESERIES  0"                                         >> ./parameters_${1}_${2}.txt
+    echo ""                                                                      >> ./parameters_${1}_${2}.txt
     
-    echo "" >> ./parameters_${1}_${3}.txt
-    echo "DATA  CHANNELS  $4" >> ./parameters_${1}_${3}.txt
-    echo "DATA  SAMPLEFREQUENCY      ${sampling}" >> ./parameters_${1}_${3}.txt
-    echo "PARAMETER CHUNKDURATION    ${chunk}" >> ./parameters_${1}_${3}.txt
-    echo "PARAMETER BLOCKDURATION    ${block}" >> ./parameters_${1}_${3}.txt
-    echo "PARAMETER OVERLAPDURATION  ${overlap}" >> ./parameters_${1}_${3}.txt
-    echo "PARAMETER FREQUENCYRANGE   ${freqmin}  ${freqmax}" >> ./parameters_${1}_${3}.txt
-    echo "PARAMETER MISMATCHMAX      ${mmmax}" >> ./parameters_${1}_${3}.txt
-    echo "TRIGGER SNRTHRESHOLD       ${snrthr}" >> ./parameters_${1}_${3}.txt
+    # Tunable parameters
+    echo "DATA       CHANNELS  $3"                                               >> ./parameters_${1}_${2}.txt
+    echo "DATA       SAMPLEFREQUENCY  ${sampling}"                               >> ./parameters_${1}_${2}.txt
+    echo "PARAMETER  CHUNKDURATION    ${chunk}"                                  >> ./parameters_${1}_${2}.txt
+    echo "PARAMETER  BLOCKDURATION    ${block}"                                  >> ./parameters_${1}_${2}.txt
+    echo "PARAMETER  OVERLAPDURATION  ${overlap}"                                >> ./parameters_${1}_${2}.txt
+    echo "PARAMETER  FREQUENCYRANGE   ${freqmin}  ${freqmax}"                    >> ./parameters_${1}_${2}.txt
+    echo "PARAMETER  MISMATCHMAX      ${mmmax}"                                  >> ./parameters_${1}_${2}.txt
+    echo "TRIGGER    SNRTHRESHOLD     ${snrthr}"                                 >> ./parameters_${1}_${2}.txt
+    echo ""                                                                      >> ./parameters_${1}_${2}.txt
 
+    # XML output
     if [ $5 -eq 1 ]; then 
-	echo "OUTPUT FORMAT       rootxml" >> ./parameters_${1}_${3}.txt
-	echo "TRIGGER CLUSTERING  TIME noroot" >> ./parameters_${1}_${3}.txt
+	echo "OUTPUT     FORMAT           rootxml"                               >> ./parameters_${1}_${2}.txt
+	echo "TRIGGER    CLUSTERING       TIME noroot"                           >> ./parameters_${1}_${2}.txt
     else
-	echo "OUTPUT FORMAT       root" >> ./parameters_${1}_${3}.txt
+	echo "OUTPUT     FORMAT           root"                                  >> ./parameters_${1}_${2}.txt
     fi
     
 } 
@@ -130,7 +169,7 @@ if [ -z "$OMICRONROOT" ]; then
 fi
 
 ##### default options
-optfile="./no.chance.this.file.exists"
+online=0
 chanfile="./no.chance.this.file.exists"
 framefile="./no.chance.this.file.exists"
 fflfile="./no.chance.this.file.exists"
@@ -138,14 +177,10 @@ outdir="./no.chance.this.dir.exists"
 outxml=0
 
 ##### read options
-while getopts ":o:c:f:d:Xh" opt; do
+while getopts ":oc:f:d:Xh" opt; do
     case $opt in
 	o)
-	    optfile="$OPTARG"
-	    if [ ! -e $optfile ]; then
-		echo "GetOmicronOptions: the option file $optfile cannot be found"
-		exit 2
-	    fi
+	    online=1
 	    ;;
 	c)
 	    chanfile="$OPTARG"
@@ -191,27 +226,12 @@ if [ ! -e $chanfile ]; then
     exit 3
 fi
 
-##### check option file
-if [ ! -e $optfile ]; then
-    echo ""
-    echo "******************************************"
-    echo "**  No template option file was given!  **"
-    echo "**  A default option file will be used  **"
-    echo "******************************************"
-    echo ""
-    if [ ! -d $outdir ]; then
-	echo "Please enter a path to an output directory: "
-	read outdir
-	outdir="${outdir}/triggers"
-    fi
+##### ffl file is required for offline analysis
+if [ $online -eq 0 ]; then # offline
     if [ ! -e $fflfile ]; then
-	echo "Please enter a path to an FFL/LCF file: "
-	read fflfile
+	echo "GetOmicronOptions: a FFL file is required"
+	exit 2
     fi
-    sed -e "s|\[FFL]|$fflfile|g" \
-	-e "s|\[OUTDIR]|$outdir|g" \
-	$OMICRON_SCRIPTS/parameters.template > ./parameters.template
-    optfile="./parameters.template"
 fi
 
 ##### user channel lists
@@ -222,7 +242,6 @@ awk '$2=="HIGH" {print $1}' $chanfile | sort | uniq          > ./channel.getomic
 awk '$2=="GW" {print $1}' $chanfile | sort | uniq            > ./channel.getomicronoptions.gw
 awk '$2=="FINE" {print $1}' $chanfile | sort | uniq          > ./channel.getomicronoptions.fine
 
-##### break into freq
 rm -f ./parameters_STD_*.txt ./parameters_STD2_*.txt ./parameters_FINE_*.txt ./parameters_HIGH_*.txt ./parameters_LOW_*.txt./parameters_GW_*.txt
 
 ############## STD SETTING
@@ -237,7 +256,7 @@ if [ -s ./channel.getomicronoptions.std ]; then
 	# this is one option file
 	if [ $n -eq $nchanmax ]; then
 	    echo "STD_${p} = $channel_list"
-	    printoption "STD" $optfile $p "$channel_list" $outxml
+	    printoption "STD" $p "$channel_list" $online $outxml
 	    channel_list=""
 	    p=$(($p+1))
 	    n=0
@@ -245,7 +264,7 @@ if [ -s ./channel.getomicronoptions.std ]; then
 	
     done < ./channel.getomicronoptions.std;
     echo "STD_${p} = $channel_list"
-    if [ $n -gt 0 ]; then printoption "STD" $optfile $p "$channel_list" $outxml; fi
+    if [ $n -gt 0 ]; then printoption "STD" $p "$channel_list" $online $outxml; fi
 fi
 
 ############## STD2 SETTING
@@ -260,7 +279,7 @@ if [ -s ./channel.getomicronoptions.std2 ]; then
 	# this is one option file
 	if [ $n -eq $nchanmax ]; then
 	    echo "STD2_${p} = $channel_list"
-	    printoption "STD2" $optfile $p "$channel_list" $outxml
+	    printoption "STD2" $p "$channel_list" $online $outxml
 	    channel_list=""
 	    p=$(($p+1))
 	    n=0
@@ -268,7 +287,7 @@ if [ -s ./channel.getomicronoptions.std2 ]; then
 	
     done < ./channel.getomicronoptions.std2;
     echo "STD2_${p} = $channel_list"
-    printoption "STD2" $optfile $p "$channel_list" $outxml
+    printoption "STD2" $p"$channel_list" $online $outxml
 fi
 
 ############## LOW SETTING
@@ -283,7 +302,7 @@ if [ -s ./channel.getomicronoptions.low ]; then
 	# this is one option file
 	if [ $n -eq $nchanmax ]; then
 	    echo "LOW_${p} = $channel_list"
-	    printoption "LOW" $optfile $p "$channel_list" $outxml
+	    printoption "LOW" $p "$channel_list" $online $outxml
 	    channel_list=""
 	    p=$(($p+1))
 	    n=0
@@ -291,7 +310,7 @@ if [ -s ./channel.getomicronoptions.low ]; then
 	
     done < ./channel.getomicronoptions.low;
     echo "LOW_${p} = $channel_list"
-    if [ $n -gt 0 ]; then printoption "LOW" $optfile $p "$channel_list" $outxml; fi
+    if [ $n -gt 0 ]; then printoption "LOW" $p "$channel_list" $online $outxml; fi
 fi
 ############## HIGH SETTING
 if [ -s ./channel.getomicronoptions.high ]; then
@@ -305,7 +324,7 @@ if [ -s ./channel.getomicronoptions.high ]; then
 	# this is one option file
 	if [ $n -eq $nchanmax ]; then
 	    echo "HIGH_${p} = $channel_list"
-	    printoption "HIGH" $optfile $p "$channel_list" $outxml
+	    printoption "HIGH" $p "$channel_list" $outxml
 	    channel_list=""
 	    p=$(($p+1))
 	    n=0
@@ -313,7 +332,7 @@ if [ -s ./channel.getomicronoptions.high ]; then
 	
     done < ./channel.getomicronoptions.high;
     echo "HIGH_${p} = $channel_list"
-    if [ $n -gt 0 ]; then printoption "HIGH" $optfile $p "$channel_list" $outxml; fi
+    if [ $n -gt 0 ]; then printoption "HIGH" $p "$channel_list" $online $outxml; fi
 fi
 ############## FINE SETTING
 if [ -s ./channel.getomicronoptions.fine ]; then
@@ -327,7 +346,7 @@ if [ -s ./channel.getomicronoptions.fine ]; then
 	# this is one option file
 	if [ $n -eq $nchanmax ]; then
 	    echo "FINE_${p} = $channel_list"
-	    printoption "FINE" $optfile $p "$channel_list" $outxml
+	    printoption "FINE" $p "$channel_list" $online $outxml
 	    channel_list=""
 	    p=$(($p+1))
 	    n=0
@@ -335,7 +354,7 @@ if [ -s ./channel.getomicronoptions.fine ]; then
 	
     done < ./channel.getomicronoptions.fine;
     echo "FINE_${p} = $channel_list"
-    if [ $n -gt 0 ]; then printoption "FINE" $optfile $p "$channel_list" $outxml; fi
+    if [ $n -gt 0 ]; then printoption "FINE" $p "$channel_list" $online $outxml; fi
 fi
 ############## GW SETTING
 if [ -s ./channel.getomicronoptions.gw ]; then
@@ -349,7 +368,7 @@ if [ -s ./channel.getomicronoptions.gw ]; then
 	# this is one option file
 	if [ $n -eq $nchanmax ]; then
 	    echo "GW_${p} = $channel_list"
-	    printoption "GW" $optfile $p "$channel_list" $outxml
+	    printoption "GW" $p "$channel_list" $online $outxml
 	    channel_list=""
 	    p=$(($p+1))
 	    n=0
@@ -357,10 +376,10 @@ if [ -s ./channel.getomicronoptions.gw ]; then
 	
     done < ./channel.getomicronoptions.gw;
     echo "GW_${p} = $channel_list"
-    if [ $n -gt 0 ]; then printoption "GW" $optfile $p "$channel_list" $outxml; fi
+    if [ $n -gt 0 ]; then printoption "GW" $p "$channel_list" $online $outxml; fi
 fi
 
 # cleaning
-rm -f ./parameters.template channel.getomicronoptions.*
+rm -f ./channel.getomicronoptions.*
 
 exit 0
