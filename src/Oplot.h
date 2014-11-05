63dd7e1253baad6b6f218e69e62210d0df97c7f0
Florent Robinet <robinet@lal.in2p3.fr>
2014-11-05 18:34:38 +0000

    ??
    git-svn-id: https://svn.ego-gw.it/svn/advsw/Omicron/trunk@57037 47e613b6-93f3-4244-b775-e06b1756610f

--- src/Oplot.h
+++ src/Oplot.h
@@ -12,7 +12,7 @@ using namespace std;
 
 /**
  * Plot Omicron triggers.
- * This class was specifically designed to plot Omicron triggers. It inherits from the TriggerPlot class. This class is actually just a wrapper of TriggerPlot as it defines 4 trigger collections differing by a SNR threshold. These collection are then assigned a given plotting style (colors, markers...).
+ * This class was specifically designed to plot Omicron triggers. It inherits from the TriggerPlot class. This class is actually just a wrapper of TriggerPlot as it defines 4 trigger collections differing by a SNR threshold. These collections are then assigned a given plotting style (colors, markers...).
  * \author Florent Robinet
  */
 class Oplot: public TriggerPlot {
@@ -25,7 +25,7 @@ class Oplot: public TriggerPlot {
   */
   /**
    * Constructor of the Oplot class.
-   * The 4 TriggerPlot collections are defined over clusters (not triggers!) with 4 different SNR thresholds and 4 different plotting styles.
+   * The 4 TriggerPlot collections are defined over TIME clusters (not triggers!) with 4 different SNR thresholds (atomatically set) and 4 different plotting styles.
    * @param aPattern input file pattern
    * @param aDirectory trigger ROOT directory
    * @param aVerbose verbosity level
@@ -40,13 +40,25 @@ class Oplot: public TriggerPlot {
      @}
   */
   
+  /**
+   * Defines a new time range for the plots.
+   * By default, the time range is given by the input trigger files.
+   * @param aTimeMin starting time
+   * @param aTimeMax stopping time
+   */ 
   void SetTimeRange(const int aTimeMin, const int aTimeMax);
 
+  /**
+   * Prints the loudest event map.
+   * After the plots have been made (see TriggerPlot::MakeCollections()), the loudest event of the plot can be mapped. The resulting map can be saved in a file if aFileName is used.
+   * @param aFileName output file name
+   */ 
   void PrintLoudestEventMap(const string aFileName="");
 
  private:
-  double snrthr[4];
-  EventMap *Eloud;
+
+  double snrthr[4];  ///< snr thresholds
+  EventMap *Eloud;   ///< loudest event map
 
   ClassDef(Oplot,0)  
 };
