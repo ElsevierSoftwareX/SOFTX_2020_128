0c40911d1cb432f4851cbb813fdb0b5fd7b20079
Florent Robinet <robinet@lal.in2p3.fr>
2016-04-25 07:03:21 +0000

    documentation
    git-svn-id: https://svn.ego-gw.it/svn/advsw/Omicron/trunk@61582 47e613b6-93f3-4244-b775-e06b1756610f

--- src/Oinject.h
+++ src/Oinject.h
@@ -20,10 +20,50 @@ class Oinject{
 
  public:
   
+  /**
+   * @name Constructors and destructors
+   @{
+  */
+  /**
+   * Constructor of the Oinject class.
+   * The ranges for the parameters are set to default values:
+   * - time: fixed at 0
+   * - frequency: 32-2048 Hz
+   * - Q: 4-100
+   * - amplitude: fixed at 1e-20
+   *
+   * A set of random parameters is generated with MakeWaveform().
+   * The user must specify the duration of the injection waveform.
+   * @param aDuration waveform duration [s]
+   */
   Oinject(const double aDuration);
-  virtual ~Oinject(void);
 
+  /**
+   * Destructor of the Oinject class.
+   */
+  virtual ~Oinject(void);
+  /**
+     @}
+  */
+
+  
+  /**
+   * Generates a new set of waveform parameters.
+   * The waveform parameters are randomly generated:
+   * - time: uniform
+   * - frequency: uniform
+   * - Q: uniform
+   * - amplitude: uniform in log
+   * - phase: uniform
+   */
   void MakeWaveform(void);
+
+  /**
+   * Returns the waveform value for a given index.
+   * The waveform value is computed according to the set of parameters previously generated with MakeWaveform(). The user must specify the frequency at which the data vector is sampled. This way, combined with the duration set in the constructor, the index can be converted to a time value.
+   * @param aIndex waveform vector index
+   * @param aSamplingFrequency sampling frequency [Hz]
+   */  
   inline double GetWaveform(const int aIndex, const int aSamplingFrequency){
     return
       amp*
@@ -31,26 +71,80 @@ class Oinject{
       TMath::Cos(2*TMath::Pi()*phi*(-duration/2.0+(double)aIndex/(double)aSamplingFrequency)+phase);// sine
   };
 
+  /**
+   * Sets a new time range.
+   * @param aTimeMin minimum time [s]
+   * @param aTimeMax maximum time [s]
+   */
   inline void SetTimeRange(const double aTimeMin, const double aTimeMax){
     taumin=aTimeMin; taumax=aTimeMax;
   };
+
+  /**
+   * Sets a new frequency range.
+   * @param aFreqMin minimum frequency [Hz]
+   * @param aFreqMax maximum frequency [Hz]
+   */
   inline void SetFrequencyRange(const double aFreqMin, const double aFreqMax){
     phimin=aFreqMin; phimax=aFreqMax;
   };
+
+  /**
+   * Sets a new amplitude range.
+   * @param aAmpMin minimum amplitude
+   * @param aAmpMax maximum amplitude
+   */
   inline void SetAmplitudeRange(const double aAmpMin, const double aAmpMax){
     ampmin=aAmpMin; ampmax=aAmpMax;
   };
+
+  /**
+   * Sets a new Q range.
+   * @param aQMin minimum Q
+   * @param aQMax maximum Q
+   */
   inline void SetQRange(const double aQMin, const double aQMax){
     Qmin=aQMin; Qmax=aQMax;
   };
 
+  /**
+   * Returns the minimum time.
+   */  
   inline double GetTimeMin(void){ return taumin; };
+
+  /**
+   * Returns the maximum time.
+   */  
   inline double GetTimeMax(void){ return taumax; };
+
+  /**
+   * Returns the minimum frequency.
+   */  
   inline double GetFrequencyMin(void){ return phimin; };
+
+  /**
+   * Returns the maximum frequency.
+   */  
   inline double GetFrequencyMax(void){ return phimax; };
+
+  /**
+   * Returns the minimum Q.
+   */  
   inline double GetQMin(void){ return Qmin; };
+
+  /**
+   * Returns the maximum Q.
+   */  
   inline double GetQMax(void){ return Qmax; };
+
+  /**
+   * Returns the minimum amplitude.
+   */  
   inline double GetAmplitudeMin(void){ return ampmin; };
+
+  /**
+   * Returns the maximum amplitude.
+   */  
   inline double GetAmplitudeMax(void){ return ampmax; };
 
 
