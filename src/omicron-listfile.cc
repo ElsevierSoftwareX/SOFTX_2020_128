83dc0131c65ee811cb8f6308dedd963a98a94dbe
Florent Robinet <robinet@lal.in2p3.fr>
2016-10-07 13:06:39 +0000

    add multiple argument lists
    git-svn-id: https://svn.ego-gw.it/svn/advsw/Omicron/trunk@62965 47e613b6-93f3-4244-b775-e06b1756610f

--- src/omicron-listfile.cc
+++ src/omicron-listfile.cc
@@ -11,26 +11,29 @@ void printhelp(void){
     cerr<<endl;
     cerr<<"Usage:"<<endl; 
     cerr<<endl;
-    cerr<<"omicron_gettriggerfile [GPS start time] [GPS stop time] [channel name]"<<endl; 
+    cerr<<"CASE1: omicron-listfile [channel name] [GPS start time] [GPS stop time]"<<endl; 
+    cerr<<"|____ prints the list of files associated to given channel between 2 GPS times."<<endl; 
+    cerr<<endl;
+    cerr<<"CASE2: omicron-listfile [channel name] [GPS time]"<<endl; 
+    cerr<<"|____ prints the file associated to a given channel containing a GPS time."<<endl; 
     cerr<<endl;
     return;
 }
 int main (int argc, char* argv[]){
 
   // check the command line
-  if(argc!=4){
+  if(argc<3){
     printhelp();
     return -1;
   }
 
-  // timing
-  int start = atoi(argv[1]);
-  int stop = atoi(argv[2]);
-  string sstart = (string)argv[1];
-  string sstop = (string)argv[2];
-
   // channel name
-  string channelname = (string)argv[3];
+  string channelname = (string)argv[1];
+
+  // timing
+  int start = atoi(argv[2]);
+  int stop=start;  
+  if(argc>3) stop = atoi(argv[3]);
 
   // trigger directory
   string trigdir = getenv("OMICRON_TRIGGERS");
