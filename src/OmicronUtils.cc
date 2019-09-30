2a1ac838d9178e01ea1acb5643d5a0e6cad463d2
Florent Robinet <robinet@lal.in2p3.fr>
2019-09-30 11:21:15 +0000

    fix bug when files are larger than 100000s
    git-svn-id: https://svn.ego-gw.it/svn/advsw/Omicron/trunk@81809 47e613b6-93f3-4244-b775-e06b1756610f

--- src/OmicronUtils.cc
+++ src/OmicronUtils.cc
@@ -26,7 +26,7 @@ string GetOmicronFilePattern(const string aChannelName, const int aTimeStart, co
   // get list of files in the first directory
   stringstream sg;
   sg<<g_start;
-  vector <string> vfiles=Glob((lv_dir.str()+"/"+sg.str()+"/"+S->GetNamePrefix()+"-*.root").c_str());
+  vector <string> vfiles=Glob((lv_dir.str()+"/*/"+S->GetNamePrefix()+"-*.root").c_str());
   sg.clear(); sg.str("");
   
   // loop over files and select the relevant ones
@@ -54,51 +54,6 @@ string GetOmicronFilePattern(const string aChannelName, const int aTimeStart, co
   }
   vfiles.clear();
   vfilefrag.clear();
-
-  
-  // loop over intermediate GPS directory
-  string idir;
-  for(int g=g_start+1; g<g_stop; g+=1){
-    sg<<g;
-    idir=lv_dir.str()+"/"+sg.str();
-    sg.clear(); sg.str("");
-    if(IsDirectory(idir)) filelist+=(idir+"/"+S->GetNamePrefix()+"-*.root ");
-  }
-
-  // last directory
-  if(g_stop!=g_start){
-
-  // get list of files in the last directory
-    sg<<g_stop;
-    vfiles=Glob((lv_dir.str()+"/"+sg.str()+"/"+S->GetNamePrefix()+"-*.root").c_str());
-    sg.clear(); sg.str("");
-
-    // loop over files and select the relevant ones
-    for(int v=0; v<(int)vfiles.size(); v++){
-      
-      // get file fragments
-      vfilefrag.clear();
-      vfilefrag = SplitString(GetFileNameFromPath(vfiles[v]),'-');
-      
-      // check file naming convention (4 fragments)
-      if(vfilefrag.size()!=4) continue;
-      
-      // file timing
-      fstart = atoi(vfilefrag[2].c_str());
-      fstop = fstart+atoi(vfilefrag[3].substr(0,vfilefrag[3].size()-5).c_str());
-      
-      // check file start time
-      if(fstart>=aTimeEnd) break;
-      
-      // check file stop time
-      if(fstop<aTimeStart) continue;
-      
-      // select file
-      filelist+=vfiles[v]+" ";
-    }
-    vfiles.clear();
-    vfilefrag.clear();
-  }
 
   return filelist;
 }
