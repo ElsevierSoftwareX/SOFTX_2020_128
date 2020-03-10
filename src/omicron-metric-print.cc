595c505f1a3f3f386dd243859739f8a6bdbc0088
Florent Robinet <robinet@lal.in2p3.fr>
2020-03-10 10:50:11 +0000

    compute metric
    git-svn-id: https://svn.ego-gw.it/svn/advsw/Omicron/trunk@82923 47e613b6-93f3-4244-b775-e06b1756610f

--- src/omicron-metric-print.cc
+++ src/omicron-metric-print.cc
@@ -29,7 +29,7 @@ void PrintUsage(void){
   cerr<<"                          The function must be given as a ROOT TFormula:"<<endl;
   cerr<<"                          https://root.cern.ch/doc/master/classTFormula.html"<<endl;
   cerr<<"                          By default trigger-function=\"pow(x, -8.0/3.0)/[0]+[1]\", corresponding"<<endl;
-  cerr<<"                          to a CBC Newtonian waveform"<<endl;
+  cerr<<"                          to a CBC Newtonian waveform. Parameters [0] and [1] are automatically set."<<endl;
   cerr<<"[first mass]              First mass m1 in solar masses. By default =1.4"<<endl;
   cerr<<"[second mass]             Second mass m2 in solar masses. By default =1.4"<<endl;
   cerr<<"[chirp mass]              Chirp mass mc in solar masses. By default it is computed using m1 and m2"<<endl;
@@ -49,7 +49,7 @@ int main (int argc, char* argv[]){
   }
 
   // list of parameters + default
-  string chname="";     // channel name
+  string chname="V1:Hrec_hoft_16384Hz";     // channel name
   string tfile_pat="";  // trigger file pattern
   double gps_start=-1;  // GPS start
   double gps_end=-1;    // GPS end
@@ -57,6 +57,7 @@ int main (int argc, char* argv[]){
   double m1=1.4;        // m1
   double m2=1.4;        // m2
   double mc=-1.0;       // mc
+  const double sun_mass=1.989e30; // kg
 
   // loop over arguments
   vector <string> sarg;
@@ -74,7 +75,7 @@ int main (int argc, char* argv[]){
   }
 
   // required time range
-  if(gps_start<0||gps_end<0){
+  if(gps_start<0||gps_end<0||gps_start>=gps_end){
     cerr<<"A trigger time range must be specified"<<endl;
       cerr<<"Type omicron-metric-print for help"<<endl;
       return 1;
@@ -82,6 +83,27 @@ int main (int argc, char* argv[]){
 
   // chirp mass
   if(mc<0) mc=pow(m1*m2,3.0/5.0)/pow(m1+m2,1.0/5.0);
+
+  // track function  
+  TF1 *func = new TF1("func",sfunc.c_str(),gps_start,gps_end);
+  if(func->GetNpar()>0)
+    func->SetParameter(0,-8.0*96.0/3.0/5.0*TMath::Power(TMath::Pi(),8.0/3.0)*TMath::Power(TMath::G()*mc*sun_mass/TMath::C()/TMath::C()/TMath::C(), 5.0/3.0));
+  if(func->GetNpar()>1)
+    func->SetParameter(1,gps_end);
+
+  // Omicron trigger files
+  if(!tfile_pat.compare("")){
+    tfile_pat=GetOmicronFilePattern(chname,gps_start-10,gps_end+10);
+  }
+
+  // trigger metric object
+  TriggerMetric *T = new TriggerMetric(tfile_pat);
+  T->ComputeMetric(func, gps_start, gps_end);
+
+  // print result
+  cout<<"Omicron metric = "<<T->GetDistanceMean()<<" +- "<<T->GetDistanceVariance()<<endl;
+  cout<<"Number of overlapping clusters = "<<T->GetNOverlappingClusters()<<endl;
+  cout<<"Number of tiles = "<<T->GetNTiles()<<endl;
 
 
   
